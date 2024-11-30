bac339475bb7dde440b789fea51856fe74b6a2c6
HooriaHashmi <hooranobviously@gmail.com>
2024-11-30 23:05:15 +0000

    Add files via upload

---
+++ solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <limits>
+#include "Final_3.cpp"
+#include "Final_2.cpp"
+
+using namespace std;
+
+int main() {
+    Health_Profile_Manager manager;
+    manager.menu();
+    return 0;
+}
