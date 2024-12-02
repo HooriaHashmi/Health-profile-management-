04942b46d1acc350a9553bde821db3b1883b9514
HooriaHashmi <hooranobviously@gmail.com>
2024-12-02 10:07:32 +0000

    Update health_project_3.cpp

--- health_project_3.cpp
+++ health_project_3.cpp
@@ -3,14 +3,14 @@
 
 #include "health_project_2.cpp" //making sure to include the previous file as inheritance will be implemented here
 
-class ExtendedHealthProfile : public HealthProfile {
+class ExtendedHealthProfile : public Health_profile {
 public: //use of inheritance to inherit the properties of the parent class which is health profile
         //here we are adding the extended health profile fields
         //The constructor is used to initialize the object's attributes
 
         
-    ExtendedHealthProfile(string name, double weight, double height, double bodyTemperature = 0.0, double bloodSugarLevel = 0.0)
-        : HealthProfile(name, weight, height, bodyTemperature, bloodSugarLevel) {}
+    ExtendedHealthProfile(string name, double weight, double height, double body_temperature = 0.0, double blood_sugar_level = 0.0)
+        : Health_profile(name, weight, height, body_temperature, blood_sugar_level) {}
 
     void advanced_analysis();
 };
