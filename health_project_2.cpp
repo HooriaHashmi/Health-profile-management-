7c46903b7acf7a3d5b1899f468ba037568d99759
HooriaHashmi <hooranobviously@gmail.com>
2024-12-02 10:06:14 +0000

    Update health_project_2.cpp

--- health_project_2.cpp
+++ health_project_2.cpp
@@ -9,46 +9,46 @@
 #include <cmath>
 using namespace std;
 
-HealthProfile::HealthProfile(string name, double weight, double height, double temperature, double sugar_level)
+Health_profile::Health_profile(string name, double weight, double height, double temperature, double sugar_level)
     : name(name), weight(weight), height(height), temperature(temperature), sugar_level(sugar_level) {}
 
-void HealthProfile::set_weight(double w) {
+void Health_profile::set_weight(double w) {
      weight = w; 
      }
-void HealthProfile::set_height(double h) { 
+void Health_profile::set_height(double h) { 
     height = h; 
     }
-void HealthProfile::set_temperature(double temp) { 
+void Health_profile::set_temperature(double temp) { 
     temperature = temp; 
     }
-void HealthProfile::set_Sugar_level(double level) { 
+void Health_profile::set_Sugar_level(double level) { 
     sugar_level = level; 
     }
 
-string HealthProfile::get_Name() const {
+string Health_profile::get_Name() const {
      return name;
     }
-double HealthProfile::get_Weight() const { 
+double Health_profile::get_Weight() const { 
     return weight; 
     }
-double HealthProfile::get_Height() const { 
+double Health_profile::get_Height() const { 
     return height; 
     }
-double HealthProfile::get_Temperature() const { 
+double Health_profile::get_Temperature() const { 
     return temperature; 
     }
-double HealthProfile::get_Sugar_Level() const { 
+double Health_profile::get_Sugar_Level() const { 
     return sugar_level; 
     }
 
-double HealthProfile::calculate_BMI() const {
+double Health_profile::calculate_BMI() const {
     return weight / pow(height, 2);
 }
 
 //This is also an extension of the original code which is included here 
 //which focuses on the calculation of the health status of the patient
 //This includes BMI temperature status and sugar level status
-string HealthProfile::bmi_category() const {
+string Health_profile::bmi_category() const {
     double bmi = calculate_BMI();
     if (bmi < 18.5) 
     return "Underweight";
@@ -60,7 +60,7 @@ string HealthProfile::bmi_category() const {
     else return "Obese";
 }
 
-string HealthProfile::temperature_status() const {
+string Health_profile::temperature_status() const {
     if (temperature < 36.1) 
     return "Low";
     else if (temperature <= 37.2) 
@@ -69,7 +69,7 @@ string HealthProfile::temperature_status() const {
     else return "High";
 }
 
-string HealthProfile::Sugar_status() const {
+string Health_profile::Sugar_status() const {
     if (sugar_level < 70) 
     return "Low";
     else if (sugar_level <= 140) 
@@ -80,7 +80,7 @@ string HealthProfile::Sugar_status() const {
 
 //This part of the code talks about the main function which is the entry point of the program
 //It deals with the creation of an object of the class HealthProfile and the execution of the methods of the class
-void HealthProfile::display_Profile() const {
+void Health_profile::display_Profile() const {
     cout << "Name: " << name << "\n"
          << "Weight: " << weight << " kg\n"
          << "Height: " << height << " m\n"
@@ -89,11 +89,11 @@ void HealthProfile::display_Profile() const {
          << "Blood Sugar: " << sugar_level << " mg/dL (" << Sugar_status() << ")\n";
 }
 
-void HealthProfile::store_Record() {
+void Health_profile::store_Record() {
     weeklyRecord.push_back("Weight: " + to_string(weight) + ", BMI: " + bmi_category());
 }
 
-void HealthProfile::display_Weekly_Records() const {
+void Health_profile::display_Weekly_Records() const {
     for (const string& record : weeklyRecord) {
         cout << record << endl;
     }
