db6be7c8dcc26314b4e35f74cf56f126ba28bf6b
HooriaHashmi <hooranobviously@gmail.com>
2024-12-02 10:10:54 +0000

    Update health_project_6.cpp
    j

--- health_project_6.cpp
+++ health_project_6.cpp
@@ -2,29 +2,29 @@
 #include <iostream>
 using namespace std;
 
-void HealthProfileManager::addProfile(const HealthProfile& profile) {
+void HealthProfileManager::add_profile(const Health_profile& profile) {
     profiles.push_back(profile);
 }
 
-void HealthProfileManager::viewProfile(const string& name) const {
+void HealthProfileManager::view_profile(const string& name) const {
     for (const auto& profile : profiles) {
         if (profile.getName() == name) {
-            profile.displayProfile();
+            profile.display_profile();
             return;
         }
     }
     cout << "Profile not found.\n";
 }
 
-void HealthProfileManager::deleteProfile(const string& name) {
+void HealthProfileManager::delete_profile(const string& name) {
     profiles.erase(remove_if(profiles.begin(), profiles.end(),
-                             [&name](const HealthProfile& p) { return p.getName() == name; }),
+                             [&name](const Health_profile& p) { return p.getName() == name; }),
                    profiles.end());
     cout << "Profile deleted.\n";
 }
 
-void HealthProfileManager::displayAllProfiles() const {
+void HealthProfileManager::display_all_profiles() const {
     for (const auto& profile : profiles) {
-        profile.displayProfile();
+        profile.display_profile();
     }
 }
